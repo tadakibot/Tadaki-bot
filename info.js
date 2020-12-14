3b93da59848e3fbec43a534acc79b73af63c4bc9
tadakibot <75942296+tadakibot@users.noreply.github.com>
2020-12-14 17:30:57 +0700

    Update info.js

--- info.js
+++ info.js
@@ -1,21 +1,21 @@
 exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🐼 ${BotName} 🐼
+	return `🤖 ${TADAKI-BOT} 🤖
   
 HALO *${id.split("@s.whatsapp.net")[0]}*
 "
-APA KABAR? SEMOGA BAIK-BAIK SAJA!
+SELAMAT DATANG DI MENU TADAKI-BOT🤖
 Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* 😁
 
 💵TOLONG BANTU DONASI AGAR BOT INI BISA TERUS BERJALAN💵
 💵GUNAKAN COMMAND !donasi untuk berdonasi💵
 
 INFO BOT! :
-🔱 *AUTHOR*: REUZ
-🔱 *GITHUB*: https://github.com/benisadewa/kntnbotwa
+🔱 *AUTHOR*: ANGGERY
+🔱 *GITHUB*: masih rahasia :v
 🔱 *INSTAGRAM*: ${instagram}
 🔱 *YOUTUBE*: ${youtube}
 🔱 *TELEGRAM* : ${telegram}
-🔱 *WHATSAPP* : 081271699856
+🔱 *WHATSAPP* : rahasia lah 
 
 🔱 *SCRIPT ORIGINAL BY*: https://github.com/fdciabdul
 
